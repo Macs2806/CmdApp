cc127d9af59bf5942e7bc4c3e676756fef9980bc
Maxim Petrusin <petrusin.max@mail.ru>
2019-04-04 13:39:07 +0600

    adding library iostream

--- CmdApp/CmdApp.cpp
+++ CmdApp/CmdApp.cpp
@@ -3,13 +3,13 @@
 #include "pch.h"
 #include "less.h"
 #include "CmdApp.h"
+#include <iostream>
 
 using namespace std;
 using namespace mySpace;
 
 int main()
 {
-	less l;
-	l.calc();
+	cout << "I include GIT" << endl;
 	return 0;
 }
